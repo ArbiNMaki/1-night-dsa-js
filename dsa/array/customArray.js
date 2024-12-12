3f9755a42f9d70f983e6146b318ab4fcfa8a4899
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-12-12 20:41:21 +0700

    Refactor Again

--- dsa/array/customArray.js
+++ dsa/array/customArray.js
@@ -3,7 +3,27 @@ class MyArray {
         this.length = 0;
         this.data = {};
     }
+
+    push (item) {
+        this.data[this.length] = item;
+        this.length++
+        return this.length;
+    }
+
+    get (index) {
+        return this.data[index];
+    }
 }
 
 const myNewArray = new MyArray();
+myNewArray.push("apple");
+myNewArray.push("banana");
+myNewArray.push("grape");
+
+console.log(myNewArray.get(0));
+console.log(myNewArray.get(1));
+console.log(myNewArray.get(2));
+
+
 console.log(myNewArray);
+console.log(myNewArray.get(0));
