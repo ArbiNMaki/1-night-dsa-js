8bb7ab452eaba5ce3d03ba2dbc8283d73c2489df
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-12-12 19:06:43 +0700

    Update log Nof Found

--- findGroceries.js
+++ findGroceries.js
@@ -1,14 +1,22 @@
 // Data Structure
-const groceries = ['milk', 'bread', 'eggs', 'flour', 'choose', 'sugar'];
+const groceries = ['milk', 'bread', 'eggs', 'flour', 'cheese', 'sugar'];
 
 // Algorithm for search
 // BigO O(n)
 const searchForItem = (item) => {
+    let found = false;
+
     for (let i = 0; i < groceries.length; i++) {
         if (groceries[i] == item) {
             console.log(`Found ${item}`);
+            found = true;
         }
+    }
+
+    if (!found) {
+        console.log("Not Found");
     }
 }
 
 searchForItem("eggs");
+searchForItem("salt");
