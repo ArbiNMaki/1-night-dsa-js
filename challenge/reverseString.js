194395c89d898161856069f86adcb7331623b3a9
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-12-12 21:51:04 +0700

    Challenge: Reverse String with built in function

---
+++ challenge/reverseString.js
@@ -0,0 +1,9 @@
+/**
+ * 1. Convert String to Array (Split Method)
+ * 2. Reverse the Array (Reverse Method)
+ * 3. Convert Array back to String (Join Method)
+ */
+
+const reverseString = (str) => str.split("").reverse().join("");
+
+console.log(reverseString("hello"));
